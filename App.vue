81cd6c1be09e1aa333d3b2470d35d0c65affba3b
김규태 <uncha@gimgyutaeui-MacBook-Pro.local>
2021-02-20 16:51:38 +0900

    app

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+    <web-view
+        :source="{uri:'http://pwm.cafe24app.com'}"
+        :style="{marginTop: 40}"
+    />
+</template>
+
+<script>
+import { WebView } from "react-native-webview";
+
+export default {
+  name: "myComponent",
+  components: {
+    "web-view": WebView
+  }
+};
+</script>
