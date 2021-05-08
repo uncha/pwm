f563df770f449c27c7423367808de744ae58fcdf
김규태 <uncha@gimgyutaeui-MacBook-Pro.local>
2021-05-08 12:41:10 +0900

    pwm camera

--- App.vue
+++ App.vue
@@ -7,11 +7,28 @@
 
 <script>
 import { WebView } from "react-native-webview";
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
 
 export default {
   name: "myComponent",
+  data () {
+      return {
+          hasCameraPermission: false,
+          type: Camera.Constants.Type.back,
+      }
+  },
   components: {
-    "web-view": WebView
-  }
+    "web-view": WebView,
+    Camera
+  },
+  mounted () {
+      Permissions.askAsync(Permissions.CAMERA)
+     .then(status => {
+       hasCameraPermission = status.status == "granted" ? true : false;
+     }).catch((err)=>{
+        console.log(err);
+     });
+  },
 };
 </script>
